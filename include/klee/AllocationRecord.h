f665cab800349293aa613097cf56ddc93cb8ecb3
David Trabish <davidtr1037@gmail.com>
2017-04-01 19:33:11 +0300

    updating

---
+++ include/klee/AllocationRecord.h
@@ -0,0 +1,39 @@
+#ifndef KLEE_ALLOCATION_RECORD_H
+#define KLEE_ALLOCATION_RECORD_H
+
+#include "klee/ASContext.h"
+
+#include <map>
+#include <queue>
+
+namespace klee {
+
+class MemoryObject;
+
+class AllocationRecord {
+public:
+
+    AllocationRecord() {
+
+    }
+
+    void addAddr(ASContext &context, MemoryObject *mo);
+    
+    MemoryObject *getAddr(ASContext &context);
+
+    bool exists(ASContext &context);
+
+    void dump();
+
+private:
+    typedef std::pair<ASContext *, std::queue<MemoryObject *> > Entry;
+    typedef std::vector<Entry> Record;
+
+    Entry *find(ASContext &context);
+
+    Record record;
+};
+
+}
+
+#endif
