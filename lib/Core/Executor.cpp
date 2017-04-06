be3d92ee1bcd414b2d17bfa3ac3772f9e62c85c7
David Trabish <davidtr1037@gmail.com>
2017-04-06 18:23:37 +0300

    workaround for suspended state removal

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -2674,6 +2674,21 @@ void Executor::executeInstruction(ExecutionState &state, KInstruction *ki) {
 
 void Executor::updateStates(ExecutionState *current) {
   if (searcher) {
+    if (!removedStates.empty()) {
+        /* we don't want to pass suspended states to the searcher */
+        std::vector<ExecutionState *> filteredStates;
+        for (std::vector<ExecutionState *>::iterator i = removedStates.begin(); i != removedStates.end(); i++) {
+            ExecutionState *removedState = *i;
+            if (removedState->isNormalState() && removedState->isSuspended()) {
+                continue;
+            }
+            filteredStates.push_back(removedState);
+        }
+        searcher->update(current, addedStates, filteredStates);
+    } else {
+        searcher->update(current, addedStates, removedStates);
+    }
+
     /* handle suspended states */
     for (std::vector<ExecutionState *>::iterator i = suspendedStates.begin(); i != suspendedStates.end(); i++) {
       searcher->removeState(*i);
@@ -2685,8 +2700,6 @@ void Executor::updateStates(ExecutionState *current) {
       searcher->addState(*i);
     }
     resumedStates.clear();
-
-    searcher->update(current, addedStates, removedStates);
   }
   
   states.insert(addedStates.begin(), addedStates.end());
@@ -4210,7 +4223,5 @@ bool Executor::isDynamicAlloc(Instruction *allocInst) {
 }
 
 void Executor::terminateDependedState(ExecutionState *dependedState) {
-    /* TODO: fix this hack... */
-    resumedStates.push_back(dependedState);
     terminateState(*dependedState);
 }
