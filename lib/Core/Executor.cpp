28bb8d1dfced6ad2a19e62bc2a309ef7211582f3
David Trabish <davidtr1037@gmail.com>
2017-04-13 18:19:11 +0300

    updating

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -439,7 +439,7 @@ const Module *Executor::setModule(llvm::Module *module,
   mra = new ModRefAnalysis(kmodule->module, ra, aa, "main", interpreterOpts.slicedFunction);
   annotator = new Annotator(kmodule->module, mra);
   cloner = new Cloner(module, ra, mra);
-  sliceGenerator = new SliceGenerator(module, aa, mra, cloner);
+  sliceGenerator = new SliceGenerator(module, aa, mra, annotator, cloner);
   kmodule->prepare(opts, interpreterHandler, ra, aa, mra, annotator, cloner, sliceGenerator);
   
   specialFunctionHandler->bind();
