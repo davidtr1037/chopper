e6e83728802130f9bf7086722ead417ac38ec02a
David Trabish <davidtr1037@gmail.com>
2017-04-23 17:23:15 +0300

    adding copy constructor and destructor to AllocationRecord

--- include/klee/AllocationRecord.h
+++ include/klee/AllocationRecord.h
@@ -17,6 +17,10 @@ public:
 
     }
 
+    AllocationRecord(const AllocationRecord &other);
+
+    ~AllocationRecord();
+
     void addAddr(ASContext &context, MemoryObject *mo);
     
     MemoryObject *getAddr(ASContext &context);
@@ -26,7 +30,7 @@ public:
     void dump();
 
 private:
-    typedef std::pair<ASContext *, std::queue<MemoryObject *> > Entry;
+    typedef std::pair<ASContext *, std::list<MemoryObject *> > Entry;
     typedef std::vector<Entry> Record;
 
     Entry *find(ASContext &context);
