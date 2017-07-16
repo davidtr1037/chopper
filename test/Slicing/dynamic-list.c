908e5305a2c62cac7449bd337d1c0113ffe049ed
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-07-16 13:52:53 +0100

    Added oracles to tests

--- test/Slicing/dynamic-list.c
+++ test/Slicing/dynamic-list.c
@@ -1,6 +1,12 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out -libc=uclibc --posix-runtime -search=dfs -skip-functions=insert_list %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-RECOVERY -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+
+// CHECK-PATHS: KLEE: done: completed paths = 1
+// CHECK-RECOVERY: KLEE: done: recovery states = 2
+// CHECK-SLICES: KLEE: done: generated slices = 2
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 1
 
 #include <stdlib.h>
 #include <assert.h>
