07d4ed2b9054bc2bd44e2e90e5621246867f7620
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-07-16 13:52:53 +0100

    Added oracles to tests

--- test/Slicing/skip-with-buffer.c
+++ test/Slicing/skip-with-buffer.c
@@ -1,3 +1,13 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out -libc=uclibc --posix-runtime -search=dfs -skip-functions=target %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-RECOVERY -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+
+// CHECK-PATHS: KLEE: done: completed paths = 1
+// CHECK-RECOVERY: KLEE: done: recovery states = 1
+// CHECK-SLICES: KLEE: done: generated slices = 1
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
