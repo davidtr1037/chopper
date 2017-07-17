c2cb171b299dea72887e114b9ecb393cfb6a6009
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-07-17 09:50:27 +0100

    Fixed test oracles

--- test/Slicing/fork.c
+++ test/Slicing/fork.c
@@ -1,7 +1,9 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out -search=dfs -skip-functions=f %t.bc > %t.out 2>&1
-// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-T -check-prefix=CHECK-F -check-prefix=CHECK-PATHS -check-prefix=CHECK-STATES -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-T
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-STATES -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-F
 
 // CHECK-T: True branch
 // CHECK-F: False branch
