f05a33d2d19c61543ce776075fdb6e7ad29d1420
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-07-17 15:52:14 +0100

    Added more tests

---
+++ test/Slicing/independent-functions.c
@@ -0,0 +1,67 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out -search=dfs -skip-functions=f,g,h %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-STATES -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-A
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-B
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-Z
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-ANOT
+
+// CHECK-PATHS: KLEE: done: completed paths = 2
+// CHECK-STATES: KLEE: done: recovery states = 2
+// CHECK-SLICES: KLEE: done: generated slices = 2
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 3
+
+// CHECK-A:a is 3
+// CHECK-B:b is 2
+// CHECK-Z:z is gt 3
+// CHECK-ANOT:a is not 3
+
+#include <stdio.h>
+#include <klee/klee.h>
+
+typedef struct {
+    int x;
+    int y;
+    int z;
+} point;
+
+void f(point *o) {
+	o->x++;
+}
+
+void g(point *o) {
+	o->y *= 2 + 1;
+}
+
+void h(point *o) {
+	o->z += 1;
+}
+
+int main(int argc, char *argv[], char *envp[]) {
+    point o;
+    o.x = 1; o.y = 1; o.z = 1;
+    int a,b;
+
+    klee_make_symbolic(&a, sizeof(a), "a");
+    klee_make_symbolic(&b, sizeof(b), "b");
+
+    f(&o);
+    if (a == 3) {
+    	printf("a is 3\n");
+    } else {
+    	printf("a is not 3\n");
+    	g(&o);
+    	if (o.x == 2) {
+    		printf("b is 2\n");
+    		h(&o);
+    		if (o.z > 0) {
+    			printf("z is gt 3\n");
+    		}
+    	} else {
+    		printf("b is not 2\n");
+    	}
+    }
+
+    return 0;
+}
---
+++ test/Slicing/multiple-allocsite-multiple-callsite.c
@@ -0,0 +1,61 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out -search=dfs -skip-functions=f %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-STATES -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-XZERO
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-XONE
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-X2ZERO
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-X2ONE
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-X3ZERO
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-X3ONE
+
+// CHECK-PATHS: KLEE: done: completed paths = 4
+// CHECK-STATES: KLEE: done: recovery states = 7
+// CHECK-SLICES: KLEE: done: generated slices = 2
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 3
+
+// CHECK-XZERO: x: 0
+// CHECK-XONE: x: 1
+// CHECK-X2ZERO: x2: 0
+// CHECK-X2ONE: x2: 1
+// CHECK-X3ZERO: x3: 0
+// CHECK-X3ONE: x3: 1
+
+#include <stdio.h>
+#include <assert.h>
+#include <klee/klee.h>
+
+typedef struct {
+    int x;
+    int y;
+} point;
+
+void f(point *o) {
+	o->x++;
+	o->y++;
+}
+
+void call(point *o, int k) {
+    f(o);
+    if (k > 0) {
+    	printf("x: %d\n", o->x);
+    	printf("x2: %d\n", o->x);
+    } else {
+    	printf("x3: %d\n", o->x);
+    }
+}
+
+int main(int argc, char *argv[], char *envp[]) {
+    point o, o2;
+    o.x = 0; o.y = 0;
+    o2.x = -1; o2.y = -1;
+    int k,j;
+
+    klee_make_symbolic(&k, sizeof(k), "k");
+    klee_make_symbolic(&j, sizeof(j), "j");
+
+    call(&o, k);
+    call(&o2, j);
+
+    return 0;
+}
