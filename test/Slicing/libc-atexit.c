7f6e1d56b267fc483e673af3a869672b04d6fdde
Andrea Mattavelli <andreamattavelli@gmail.com>
2017-07-17 14:36:00 +0100

    Added simple test to check the correct use of klee-libc and uclibc

---
+++ test/Slicing/libc-atexit.c
@@ -0,0 +1,24 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --libc=klee -search=dfs -skip-functions=foo %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-RECOVERY -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --libc=uclibc -search=dfs -skip-functions=foo %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-RECOVERY -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+
+// CHECK-PATHS: KLEE: done: completed paths = 1
+// CHECK-STATES: KLEE: done: recovery states = 0
+// CHECK-SLICES: KLEE: done: generated slices = 0
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 1
+
+
+#include<stdio.h>
+
+void foo() {
+	printf("At exit\n");
+}
+
+int main() {
+  atexit(foo);
+  return 0;
+}
