1e1fcb1fa8b4eab302be36965921dfba6b2ebd59
David Trabish <davidtr1037@gmail.com>
2017-08-07 22:14:02 +0300

    adding tests for arrays

--- test/Slicing/array-5.c
+++ test/Slicing/array-5.c
@@ -1,14 +1,14 @@
 // RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out -search=dfs -skip-functions=f,g %t.bc > %t.out 2>&1
+// RUN: %klee --output-dir=%t.klee-out -search=dfs -skip-functions=f %t.bc > %t.out 2>&1
 // RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-STATES -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
 // RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-A
 // RUN: not FileCheck %s -input-file=%t.out -check-prefix=CHECK-B
 
 // CHECK-PATHS: KLEE: done: completed paths = 1
-// CHECK-STATES: KLEE: done: recovery states = 2
-// CHECK-SLICES: KLEE: done: generated slices = 2
-// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 2
+// CHECK-STATES: KLEE: done: recovery states = 0
+// CHECK-SLICES: KLEE: done: generated slices = 0
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 1
 // CHECK-A: Correct
 // CHECK-B: Incorrect
 
@@ -17,21 +17,16 @@
 
 #include <klee/klee.h>
 
-void f(short *buffer) {
-    buffer[0] = 1000;
-}
-
-void g(short *buffer) {
-    buffer[1] = 2000;
+void f(unsigned char *buffer) {
+    buffer[0] = 7;
 }
 
 int main(int argc, char *argv[], char *envp[]) {
-    short buffer[10] = {0,};
+    unsigned char buffer[10] = {0,};
 
     f(buffer);
-    g(buffer);
-
-    if (buffer[0] == 1000) {
+    memset(buffer, 1, sizeof(buffer));
+    if (buffer[0] == 1) {
         printf("Correct\n");
     } else {
         printf("Incorrect\n");
