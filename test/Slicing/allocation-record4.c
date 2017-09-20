184dc6fbc5fe33ebcd06233d441c620873fcddb7
David Trabish <davidtr1037@gmail.com>
2017-09-20 09:10:33 +0300

    adding test

---
+++ test/Slicing/allocation-record4.c
@@ -0,0 +1,45 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t.bc
+// RUN: %opt -mem2reg %t.bc -o %t.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out -debug-only=basic -search=dfs -skip-functions=f %t.bc > %t.out 2>&1
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-PATHS -check-prefix=CHECK-RECOVERY -check-prefix=CHECK-SLICES -check-prefix=CHECK-SNAPSHOTS
+// RUN: FileCheck %s -input-file=%t.out -check-prefix=CHECK-A
+// RUN: not FileCheck %s -input-file=%t.out -check-prefix=CHECK-B
+
+// CHECK-PATHS: KLEE: done: completed paths = 1
+// CHECK-RECOVERY: KLEE: done: recovery states = 6
+// CHECK-SLICES: KLEE: done: generated slices = 2
+// CHECK-SNAPSHOTS: KLEE: done: created snapshots = 2
+
+// CHECK-A: Correct
+// CHECK-B: Incorrect
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAGIX_X (100)
+
+typedef struct {
+    int x;
+} object_t;
+
+object_t *g_object = NULL;
+
+void f() {
+    if (g_object == NULL) {
+        g_object = malloc(sizeof(*g_object));
+        g_object->x = MAGIX_X;
+    }
+}
+
+int main(int argc, char *argv[], char *envp[]) {
+    f();
+    f();
+    if (g_object->x == MAGIX_X) {
+        printf("Correct\n");
+    } else {
+        printf("Incorrect\n");
+    }
+
+	return 0;
+}
